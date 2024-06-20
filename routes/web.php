fcad32320c6ca5cdd934c44d9931b425dd341fb7
ariannelizg <aig4561@students.uc-bcf.edu.ph>
2024-06-20 22:22:46 +0800

    Created controller and blade.php files

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
+use App\Http\Controllers\AboutMeController;
+use App\Http\Controllers\HobbiesController;
+use App\Http\Controllers\SkillsController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/about', [AboutMeController::class, 'aboutMe']);
+
+Route::get('/skills', [SkillsController::class, 'mySkills']);
+
+Route::get('/hobbies', [HobbiesController::class, 'myHobbies']);
