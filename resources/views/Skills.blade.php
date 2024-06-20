bc0ef2e1ec56b752d0c2df70ad481a8796f239db
ariannelizg <aig4561@students.uc-bcf.edu.ph>
2024-06-20 22:32:51 +0800

    Added contents in Skills.blade.php

--- resources/views/Skills.blade.php
+++ resources/views/Skills.blade.php
@@ -1,3 +1,54 @@
-<div>
-    <!-- An unexamined life is not worth living. - Socrates -->
-</div>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About Me</title>
+    <style>
+        .skillscontents {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            font-family: Arial, sans-serif;
+        }
+
+        .button{
+            display: flex;
+            padding: 10px;
+            justify-content: center;
+            align-items: center;
+        }
+        a {
+            text-decoration: none;
+            font-size: 20px;
+            color: #333;
+        }
+
+    </style>
+</head>
+
+<body>
+    <section class="skillscontents">
+        <div>
+            <h1>Skills</h1>
+            <ul>
+                <li>{{ $first }}</li>
+                <li>{{ $second }}</li>
+                <li>{{ $third }}</li>
+                <li>{{ $fourth }}</li>
+                <li>{{ $fifth }}</li>
+                <li>{{ $sixth }}</li>
+                <li>{{ $seventh }}</li>
+            </ul>
+        </div> 
+     </section>
+        
+     <section class='button'>
+         <div>
+             <button><a href="/about">ABOUT ME</a></button>
+             <button><a href="/skills">SKILLS</a></button>
+             <button><a href="/hobbies">HOBBIES</a></button>
+         </div>
+     </section>
+</body>
+</html>
