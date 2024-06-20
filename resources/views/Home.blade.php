a87040ace49a8d1c9886de9bf28f55e770bc071c
ariannelizg <aig4561@students.uc-bcf.edu.ph>
2024-06-20 22:32:15 +0800

    Added contents in Home.blade.php

--- resources/views/Home.blade.php
+++ resources/views/Home.blade.php
@@ -1,3 +1,46 @@
-<div>
-    <!-- Breathing in, I calm body and mind. Breathing out, I smile. - Thich Nhat Hanh -->
-</div>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home Page</title>
+    <style>
+        .homecontents {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            font-family: Arial, sans-serif;
+        }
+
+        .button{
+            display: flex;
+            padding: 10px;
+            justify-content: center;
+            align-items: center;
+        }
+        a {
+            text-decoration: none;
+            font-size: 20px;
+            color: #333;
+        }
+
+    </style>
+</head>
+
+<body>
+    <section class='homecontents'>
+        <div>
+            <h1>HELLO, WELCOME TO ARIANNE'S INFORMATION</h1>
+            <p> Click a button below to start knowing Arianne</p>
+        </div> 
+     </section>
+        
+     <section class='button'>
+         <div>
+             <button><a href="/about">ABOUT ME</a></button>
+             <button><a href="/skills">SKILLS</a></button>
+             <button><a href="/hobbies">HOBBIES</a></button>
+         </div>
+     </section>
+</body>
+</html>
